72ff07b45e1db01dbdeb8d00aa7071e88ed0d4e5
DianaEvseevaBSU2019 <58406088+DianaEvseeva@users.noreply.github.com>
2019-12-19 10:08:59 +0300

    Add files via upload

---
+++ Woody_java/src/test/java/by/fsc/showcase/ShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package by.fsc.showcase;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
